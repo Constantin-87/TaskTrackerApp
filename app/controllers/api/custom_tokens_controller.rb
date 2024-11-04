90245c79243adf02947ad8b6daa18c87e1504b9a
Constantin Gavrila <Constantin.Gavrila@citywonders.com>
2024-11-04 15:37:17 +0000

    added logging

--- app/controllers/api/custom_tokens_controller.rb
+++ app/controllers/api/custom_tokens_controller.rb
@@ -4,7 +4,12 @@ module Api
 
     # Define the custom sign_up_params method to permit required parameters and convert to hash
     def sign_up_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation).to_h
+      permitted_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation).to_h
+
+      # Add logs to inspect the permitted parameters
+      Rails.logger.info "Sign up parameters: #{permitted_params.inspect}"
+
+      permitted_params
     end
   end
 end
