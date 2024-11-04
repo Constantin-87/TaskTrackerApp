7d8e8104251b43e2c41efb6bbcf1b2b22bb1bdb6
Constantin Gavrila <Constantin.Gavrila@citywonders.com>
2024-11-04 23:06:52 +0000

    lint fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,10 +14,10 @@ Devise.setup do |config|
   config.navigational_formats = []  # No HTML views, only API (JSON)
 
   # Authorization
-  api.authorization.key = 'Authorization'
-  api.authorization.scheme = 'Bearer'
+  api.authorization.key = "Authorization"
+  api.authorization.scheme = "Bearer"
   api.authorization.location = :both # :header or :params or :both
-  api.authorization.params_key = 'access_token'
+  api.authorization.params_key = "access_token"
 
   config.password_length = 6..128
 end
