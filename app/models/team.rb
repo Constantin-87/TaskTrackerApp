43b13869aae6099e1bf8c4f809ea12577e32f2b4
Constantin Gavrila <Constantin.Gavrila@citywonders.com>
2024-11-05 13:54:25 +0000

    fixed teams model

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,7 @@ class Team < ApplicationRecord
     has_many :boards, dependent: :nullify
 
     validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
-    validates :description, presence: true, { minimum: 20, maximum: 500 }
+    validates :description, presence: true, length: { minimum: 20, maximum: 500 }
 
     def users_count
         users.size
